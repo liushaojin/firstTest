99aea8e893a47546f57a4fef991e2b6d3a9a695e
liushaojin <474007782@qq.com>
2017-09-30 17:38:15 +0800

    解决发送升级数据后不能重启的问题

--- WindowsApplication1/Form2.Designer.cs
+++ WindowsApplication1/Form2.Designer.cs
@@ -28,77 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.setTtabPage = new System.Windows.Forms.TabPage();
+            this.setTabControl = new System.Windows.Forms.TabControl();
+            this.devPage = new System.Windows.Forms.TabPage();
+            this.framePage = new System.Windows.Forms.TabPage();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.tabControl1.SuspendLayout();
-            this.setTtabPage.SuspendLayout();
+            this.userSetPage = new System.Windows.Forms.TabPage();
+            this.setTabControl.SuspendLayout();
+            this.framePage.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tabControl1
+            // setTabControl
             // 
-            this.tabControl1.Alignment = System.Windows.Forms.TabAlignment.Left;
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.setTtabPage);
-            this.tabControl1.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
-            this.tabControl1.Location = new System.Drawing.Point(26, 65);
-            this.tabControl1.Multiline = true;
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(200, 100);
-            this.tabControl1.TabIndex = 0;
+            this.setTabControl.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.setTabControl.Controls.Add(this.devPage);
+            this.setTabControl.Controls.Add(this.framePage);
+            this.setTabControl.Controls.Add(this.userSetPage);
+            this.setTabControl.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+            this.setTabControl.Location = new System.Drawing.Point(0, 0);
+            this.setTabControl.Multiline = true;
+            this.setTabControl.Name = "setTabControl";
+            this.setTabControl.SelectedIndex = 0;
+            this.setTabControl.Size = new System.Drawing.Size(580, 440);
+            this.setTabControl.TabIndex = 0;
             // 
-            // tabPage1
+            // devPage
             // 
-            this.tabPage1.Location = new System.Drawing.Point(40, 4);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(156, 92);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.devPage.Location = new System.Drawing.Point(22, 4);
+            this.devPage.Name = "devPage";
+            this.devPage.Padding = new System.Windows.Forms.Padding(3);
+            this.devPage.Size = new System.Drawing.Size(554, 432);
+            this.devPage.TabIndex = 0;
+            this.devPage.Text = "设备区";
+            this.devPage.UseVisualStyleBackColor = true;
             // 
-            // setTtabPage
+            // framePage
             // 
-            this.setTtabPage.Controls.Add(this.groupBox1);
-            this.setTtabPage.Location = new System.Drawing.Point(40, 4);
-            this.setTtabPage.Name = "setTtabPage";
-            this.setTtabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.setTtabPage.Size = new System.Drawing.Size(156, 92);
-            this.setTtabPage.TabIndex = 1;
-            this.setTtabPage.Text = "设置标签页";
-            this.setTtabPage.UseVisualStyleBackColor = true;
+            this.framePage.Controls.Add(this.groupBox1);
+            this.framePage.Location = new System.Drawing.Point(22, 4);
+            this.framePage.Name = "framePage";
+            this.framePage.Padding = new System.Windows.Forms.Padding(3);
+            this.framePage.Size = new System.Drawing.Size(554, 432);
+            this.framePage.TabIndex = 1;
+            this.framePage.Text = "报文区";
+            this.framePage.UseVisualStyleBackColor = true;
             // 
             // groupBox1
             // 
-            this.groupBox1.Location = new System.Drawing.Point(71, 39);
+            this.groupBox1.Location = new System.Drawing.Point(6, 3);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(79, 50);
+            this.groupBox1.Size = new System.Drawing.Size(545, 130);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
+            // 
+            // userSetPage
+            // 
+            this.userSetPage.Location = new System.Drawing.Point(22, 4);
+            this.userSetPage.Name = "userSetPage";
+            this.userSetPage.Size = new System.Drawing.Size(554, 432);
+            this.userSetPage.TabIndex = 2;
+            this.userSetPage.Text = "用户设置";
+            this.userSetPage.UseVisualStyleBackColor = true;
             // 
             // SetForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.tabControl1);
+            this.ClientSize = new System.Drawing.Size(584, 442);
+            this.Controls.Add(this.setTabControl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "SetForm";
             this.Text = "设置";
             this.Load += new System.EventHandler(this.SetForm_Load);
-            this.tabControl1.ResumeLayout(false);
-            this.setTtabPage.ResumeLayout(false);
+            this.setTabControl.ResumeLayout(false);
+            this.framePage.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage setTtabPage;
+        private System.Windows.Forms.TabControl setTabControl;
+        private System.Windows.Forms.TabPage devPage;
+        private System.Windows.Forms.TabPage framePage;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TabPage userSetPage;
     }
 }
--- WindowsApplication1/Form2.cs
+++ WindowsApplication1/Form2.cs
@@ -17,7 +17,20 @@ namespace WindowsApplication1
 
         private void SetForm_Load(object sender, EventArgs e)
         {
+            setTabControl.DrawItem += new DrawItemEventHandler(this.SetTabPageDrawItem);
+        }
 
+        private void SetTabPageDrawItem(object sender, DrawItemEventArgs e)
+        {
+            Rectangle tabArea = setTabControl.GetTabRect(e.Index);//主要是做个转换来获得TAB项的RECTANGELF 
+            RectangleF tabTextArea = (RectangleF)(setTabControl.GetTabRect(e.Index));
+            Graphics g = e.Graphics;
+            StringFormat sf = new StringFormat();//封装文本布局信息 
+            sf.LineAlignment = StringAlignment.Center;
+            sf.Alignment = StringAlignment.Near;
+            Font font = this.setTabControl.Font;
+            SolidBrush brush = new SolidBrush(Color.Black);//绘制边框的画笔 
+            g.DrawString(((TabControl)(sender)).TabPages[e.Index].Text, font, brush, tabTextArea, sf);
         }
     }
 }
